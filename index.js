046c572b2bd8fbaba3dcff18b9ca49556bf04b41
Kaamibroken <kaamibroken64@gmail.com>
2026-03-07 04:12:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 3000;
 // --- IMPORT ALL PANELS ---
 const goat = require("./api/goat");
 const np = require("./api/np");
+const ts = require("./api/ts");
 const msi = require("./api/msi");
 const roxy = require("./api/roxy");  // <-- NEW
 
 // --- ROUTES ---
 app.use("/api/goat", goat);
 app.use("/api/np", np);
+app.use("/api/ts", ts);
 app.use("/api/msi", msi);
 app.use("/api/roxy", roxy); // <-- NEW
 
